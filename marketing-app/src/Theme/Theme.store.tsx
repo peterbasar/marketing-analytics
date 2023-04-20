b1d6ce749dd966e108021e9883abdc01d572596a
PeterKillerio <peter.basar0@gmail.com>
2023-04-20 21:25:03 +0200

    <NEW> Gradients added to theme

--- marketing-app/src/Theme/Theme.store.tsx
+++ marketing-app/src/Theme/Theme.store.tsx
@@ -2,6 +2,7 @@ export interface ThemeHexColorInterface {
     primary: string,
     secondary: string,
     tertiary: string,
+    text: string,
 }
 interface RgbValuesInterface {
     r: number,
@@ -12,12 +13,17 @@ interface ThemeRgbColorInterface {
     primary: RgbValuesInterface,
     secondary: RgbValuesInterface,
     tertiary: RgbValuesInterface,
+    text: RgbValuesInterface,
 }
 
+interface GradientsInterface {
+    primary: string,
+}
 
 export interface ThemeDataInterface {
     hex: ThemeHexColorInterface,
     rgb: ThemeRgbColorInterface,
+    gradient: GradientsInterface,
 }
 
 
@@ -26,10 +32,15 @@ export const DefaultTheme: ThemeDataInterface = {
         primary: "#D9D9D9",
         secondary: "#FFFFFF",
         tertiary: "#88C6FF",
+        text: "#000000"
     },
     rgb: {
         primary: {r: 217, g: 217, b: 217},
         secondary: {r: 255, g: 255, b: 255},
         tertiary: {r: 136, g: 198, b: 255},
+        text: {r: 0, g: 0, b: 0},
+    },
+    gradient: {
+        primary: "linear-gradient(315deg, rgba(217,217,217,1) 0%, rgba(255,255,255,1) 100%)",
     }
 }
--- marketing-app/src/Theme/Theme.tsx
+++ marketing-app/src/Theme/Theme.tsx
@@ -12,12 +12,17 @@ export const ThemeContext = React.createContext<ThemeDataInterface>(DefaultTheme
 
 const Theme = ({children}: ThemeInterface) => {
     /* Set theme CSS accessible variables */
-    const variables = Object.keys(DefaultTheme.hex);
     useEffect(() => {
-        for (let i=0; i < variables.length; i++) {
-            // @ts-ignore
-            document.documentElement.style.setProperty(`--${variables[i]}`, DefaultTheme.hex[variables[i] as string]);
-        } 
+        /* Hex */
+        const hexVariables = Object.keys(DefaultTheme.hex);
+        for (let i=0; i < hexVariables.length; i++) { // @ts-ignore
+            document.documentElement.style.setProperty(`--hex-${hexVariables[i]}`, DefaultTheme.hex[hexVariables[i] as string]);
+        }
+        /* Gradient */
+        const gradientVariables = Object.keys(DefaultTheme.gradient);
+        for (let i=0; i < gradientVariables.length; i++) { // @ts-ignore
+            document.documentElement.style.setProperty(`--gradient-${gradientVariables[i]}`, DefaultTheme.gradient[gradientVariables[i] as string]);
+        }
     })
 
     /* Provide theme context */
