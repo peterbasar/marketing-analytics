3cccf919d143747b2986aa809a3f79bf66f173b4
PeterKillerio <peter.basar0@gmail.com>
2023-04-21 17:45:57 +0200

    <EDIT> PartitionPage improved

--- marketing-app/src/Pages/PartitionPage/PartitionPage.tsx
+++ marketing-app/src/Pages/PartitionPage/PartitionPage.tsx
@@ -1,17 +1,47 @@
 import React from "react";
 import "Pages/PartitionPage/PartitionPage.css"
+import { useNavigate } from "react-router-dom";
 /* Components */
 import Container from "Components/Container/Container";
 import Sidebar from "Components/Sidebar/Sidebar";
 import BasicContentWrapper from "Components/BasicContentWrapper/BasicContentWrapper";
 import BasicPageWrapper from "Components/BasicPageWrapper/BasicPageWrapper";
+import Card from "Components/Card/Card";
+import CardBox from "Components/CardBox/CardBox";
+import PartitionCard from "Components/PartitionCard/PartitionCard";
+/* Constants */
+import { FRONTEND_ENDPOINTS } from "config";
+/* Zustand */
+import { useDataManagerStore } from "Components/DataManager/DataManager.store";
 
 
 const PartitionPage = () => {
+    /* Zustand */
+    const navigate = useNavigate();
+    const paritions = useDataManagerStore((state) => state.partitions)
+    const setSelectedPartition = useDataManagerStore((state) => state.setSelectedPartition)
+
+
     return (
         <BasicPageWrapper>
             <BasicContentWrapper heading="partition_page_heading">
-                Content children
+                <CardBox>
+                    {   paritions.map((item) => {
+                            return (
+                                <PartitionCard
+                                    key={item.id}
+                                    country={item.country}
+                                    brand={item.brand}
+                                    id={item.id}
+                                    onClick={() => {
+                                        setSelectedPartition(item)
+                                        navigate(FRONTEND_ENDPOINTS.DASHBOARD)
+                                    }}
+                                />                   
+                            )
+                        })
+                    }
+                </CardBox>
             </BasicContentWrapper>
         </BasicPageWrapper>
     )
