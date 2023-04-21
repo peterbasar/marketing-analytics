f0ad278d4ac30329e935c696f364875ed03f74e4
PeterKillerio <peter.basar0@gmail.com>
2023-04-21 17:44:34 +0200

    <NEW> Icons

---
+++ marketing-app/src/Assets/Icons/index.tsx
@@ -0,0 +1,3 @@
+export { ReactComponent as PersonIcon } from './person.svg';
+export { ReactComponent as GridIcon } from './grid.svg';
+export { ReactComponent as AnalyticsIcon } from './analytics.svg';
