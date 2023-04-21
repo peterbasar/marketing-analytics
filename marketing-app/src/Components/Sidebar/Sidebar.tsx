0c35520d0bb507781f63259c99fc94bb990ad08b
PeterKillerio <peter.basar0@gmail.com>
2023-04-21 12:57:29 +0200

    <NEW> Sidebar base

---
+++ marketing-app/src/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "Components/Sidebar/Sidebar.css"
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar-wrapper">
+            
+        </div>
+    )
+}
+export default Sidebar
