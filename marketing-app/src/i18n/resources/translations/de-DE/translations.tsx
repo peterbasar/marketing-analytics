738626bff0a25ae0d834c5eddba38a7da59f7638
PeterKillerio <peter.basar0@gmail.com>
2023-04-25 09:48:12 +0200

    <EDIT> Translation

--- marketing-app/src/i18n/resources/translations/de-DE/translations.tsx
+++ marketing-app/src/i18n/resources/translations/de-DE/translations.tsx
@@ -3,6 +3,78 @@ import { translationInterface } from "i18n/resources/translations/interface"
 
 export const translations = {
     translation: {
-        "enter_your_api_key": "hello world enDE",
+        /* Page link names */
+        set_api_link: "Legen Sie Ihren API-Schlüssel fest",
+        select_partition_link: "Wählen Sie Ihre Partition aus",
+        dashboard_link: "Armaturenbrett",
+        partition_data_link: "Daten partitionieren",
+
+        set_valid_api_key_first: "Legen Sie zuerst einen gültigen API-Schlüssel fest",
+        enter_your_api_key: "Geben Sie Ihren API-Schlüssel ein",
+        current_model: "Aktuelles Datenmodell",
+
+        /* Headings */
+        dashboard_page_heading: "Armaturenbrett",
+        partition_page_heading: "Verfügbare Partitionen",
+        partition_data_page_heading: "Partitionsdaten",
+        
+        /* Buttons */
+        continue: "Weiter",
+        select: "Auswählen",
+        
+        /* Charts and tables */
+        performance_report_table: "Leistungsbericht",
+        partition_data_table: "Partitionsdaten",
+        spending_and_revenue_chart_heading: "Ausgaben & Einnahmen",
+        roas_and_cpa_chart_heading: "ROAS & CPA",
+        spending: "Ausgaben",
+        partition: "Einnahmen",
+        roas: "ROAS",
+        cpa: "CPA",
+        conversions: "Konvertierungen",
+        incrementality: "Bezahlte Kanäle",
+        spend: "Ausgegeben",
+        revenue: "Einnahmen",
+
+        /* Date range */
+        date_range: "Datumsbereich",
+        date_range_start: "Startdatum",
+        date_range_end: "Enddatum",
+
+        /* Column names */
+        brand: "Name",
+        id: "ID", 
+        country: "Land",
+        column_date: "Datum",
+        column_source: "Quelle",
+        column_source_type: "Quelle Typ",
+        column_partition_id: "Partition ID",
+        column_optimisation_target: "Modelltyp",
+        column_revenue: "Einnahmen",
+        column_conversions: "Konvertierungen",
+        column_spend: "Ausgaben",
+        column_roas: "ROAS",
+        column_cpa: "CPA",
+
+        /* Source names */
+        affiliate_prospecting: "Affiliate-Prospektion",
+        baseline: "Grundlinie",
+        direct: "Direkte",
+        display_prospecting: "Prospektion anzeigen",
+        display_retargeting: "Retargeting anzeigen",
+        google_performance: "Google-Leistung",
+        organicsearch: "Organische Suche",
+        organicsocial: "Organisch sozial",
+        paidsearch_brand: "Bezahlte Suche - Name",
+        paidsearch_dynamic_search_ads_prospecting: "Bezahlte Suche - Anzeigen Prospektion",
+        paidsearch_generic_prospecting: "Bezahlte Suche - generische Prospektion",
+        paidsocial_prospecting: "Bezahlte soziale Medien - Prospektion",
+        paidsocial_retargeting: "Bezahlte soziale Medien - Retargeting",
+        radio: "Radio",
+        tv: "Fernsehen",
+
+        /* Languages */
+        "en-US": "EN",
+        "de-DE": "DE",
     }
 }
--- marketing-app/src/i18n/resources/translations/en-US/translations.tsx
+++ marketing-app/src/i18n/resources/translations/en-US/translations.tsx
@@ -4,20 +4,25 @@ import { translationInterface } from "i18n/resources/translations/interface"
 export const translations = {
     translation: {
         /* Page link names */
-        select_api_link: "Select API",
-        select_partition_link: "Select Partition",
+        set_api_link: "Set your API key",
+        select_partition_link: "Select your partition",
         dashboard_link: "Dashboard",
-        partition_data_link: "Partition Data",
+        partition_data_link: "Partition data",
 
+        set_valid_api_key_first: "Set valid API key first",
+        enter_your_api_key: "Enter your API key",
+        current_model: "Current data model",
+
+        /* Headings */
         dashboard_page_heading: "Dashboard",
         partition_page_heading: "Available Partitions",
         partition_data_page_heading: "Partition Data",
         
-        enter_your_api_key: "Enter your API key",
-
+        /* Buttons */
         continue: "Continue",
         select: "Select",
         
+        /* Charts and tables */
         performance_report_table: "Performance report",
         partition_data_table: "Partition data",
         spending_and_revenue_chart_heading: "Spending & Revenue",
@@ -28,8 +33,10 @@ export const translations = {
         cpa: "CPA",
         conversions: "Conversions",
         incrementality: "Paid channels",
+        spend: "Spent",
+        revenue: "Revenue",
 
-
+        /* Date range */
         date_range: "Date range",
         date_range_start: "Start date",
         date_range_end: "End date",
@@ -58,7 +65,7 @@ export const translations = {
         google_performance: "Google performance",
         organicsearch: "Organic search",
         organicsocial: "Organic social",
-        paidsearch_brand: "Paid search brand",
+        paidsearch_brand: "Paid search - brand",
         paidsearch_dynamic_search_ads_prospecting: "Paid search - ads prospecting",
         paidsearch_generic_prospecting: "Paid search - generic prospecting",
         paidsocial_prospecting: "Paid social - prospecting",
@@ -66,13 +73,8 @@ export const translations = {
         radio: "Radio",
         tv: "Television",
 
-        /* Variables */
-        spend: "Spent",
-        revenue: "Revenue",
-
         /* Languages */
         "en-US": "EN",
         "de-DE": "DE",
-
     }
 }
