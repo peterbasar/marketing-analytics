4c085547f5a7e49250610df0f9f548981a83b605
PeterKillerio <peter.basar0@gmail.com>
2023-04-25 02:46:05 +0200

    <NEW> New layout and new charts in DashboardPage

--- marketing-app/src/Pages/DashboardPage/DashboardPage.tsx
+++ marketing-app/src/Pages/DashboardPage/DashboardPage.tsx
@@ -15,6 +15,9 @@ import DashboardContainer from "Components/DashboardContainer/DashboardContainer
 import DashboardItem from "Components/DashboardItem/DashboardItem"
 import PerformanceReportTable from "Components/Tables/PerformanceReportTable";
 import RevenueSpendBarChart from "Components/Charts/RevenueSpendBarChart/RevenueSpendBarChart";
+import CustomPieChart from "Components/Charts/CustomPieChart/CustomPieChart";
+import RoasCpaBarChart from "Components/Charts/RoasCpaBarChart/RoasCpaBarChart";
+import ConversionsSankeyChart from "Components/Charts/ConversionsSankeyChart/ConversionsSankeyChart";
 /* Zustand */
 import { useDataManagerStore } from "Components/DataManager/DataManager.store";
 
@@ -24,7 +27,9 @@ const DashboardPage = () => {
     const { t } = useTranslation()
     /* Zustand */
     const selectedPartition = useDataManagerStore((state) => state.selectedPartition)
-
+    const performanceReportData = useDataManagerStore((state) => state.performanceReportData)
+    const selectedPerformanceReportData = useDataManagerStore((state) => state.selectedPerformanceReportData)
+    const selectedSources = useDataManagerStore((state) => state.selectedSources)
 
     return (
         <BasicPageWrapper>
@@ -34,6 +39,7 @@ const DashboardPage = () => {
                     <DashboardBar />
 
                     <DashboardContainer>
+                        
                         <DashboardItem
                             heading={`${t("performance_report_table")}`}
                             behavior={{
@@ -47,16 +53,118 @@ const DashboardPage = () => {
                         </DashboardItem>
 
                         <DashboardItem
+                            heading={`${t("roas_and_cpa_chart_heading")}`}
+                            behavior={{
+                                lg: {row: 1,col: 4},
+                                md: {row: 2,col: 4},
+                                sm: {row: 2,col: 4},
+                                default: {row: 3,col: 4},
+                            }}
+                        >
+                            <RoasCpaBarChart />
+                        </DashboardItem>
+
+                        <DashboardItem
+                            heading={`${t("roas")}`}
+                            behavior={{
+                                lg: {row: 2,col: 2},
+                                md: {row: 2,col: 2},
+                                sm: {row: 4,col: 4},
+                                default: {row: 4,col: 4},
+                            }}
+                        >
+                            <CustomPieChart
+                                data={ (() => { 
+                                    return selectedPerformanceReportData.map((row) => {
+                                        return { name: row.source, value: Math.round(row.roas*100)/100 }
+                                    })
+                                })()
+                                }
+                            />
+                        </DashboardItem>
+
+                        <DashboardItem
+                            heading={`${t("cpa")}`}
+                            behavior={{
+                                lg: {row: 2,col: 2},
+                                md: {row: 2,col: 2},
+                                sm: {row: 4,col: 4},
+                                default: {row: 4,col: 4},
+                            }}
+                        >
+                            <CustomPieChart
+                                data={ (() => { 
+                                    return selectedPerformanceReportData.map((row) => {
+                                        return { name: row.source, value: Math.round(row.cpa*100)/100 }
+                                    })
+                                })()
+                                }
+                            />
+                        </DashboardItem>
+
+                        <DashboardItem
                             heading={`${t("spending_and_revenue_chart_heading")}`}
                             behavior={{
                                 lg: {row: 1,col: 4},
                                 md: {row: 2,col: 4},
                                 sm: {row: 2,col: 4},
-                                default: {row: 2,col: 4},
+                                default: {row: 3,col: 4},
                             }}
                         >
                             <RevenueSpendBarChart />
                         </DashboardItem>
+
+                        <DashboardItem
+                            heading={`${t("spending")}`}
+                            behavior={{
+                                lg: {row: 2,col: 2},
+                                md: {row: 2,col: 2},
+                                sm: {row: 4,col: 4},
+                                default: {row: 4,col: 4},
+                            }}
+                        >
+                            <CustomPieChart
+                                data={ (() => { 
+                                    return selectedPerformanceReportData.map((row) => {
+                                        return { name: row.source, value: Math.round(row.spend) }
+                                    })
+                                })()
+                                }
+                            />
+                        </DashboardItem>
+
+                        <DashboardItem
+                            heading={`${t("revenue")}`}
+                            behavior={{
+                                lg: {row: 2,col: 2},
+                                md: {row: 2,col: 2},
+                                sm: {row: 4,col: 4},
+                                default: {row: 4,col: 4},
+                            }}
+                        >
+                            <CustomPieChart
+                                data={ (() => { 
+                                    return selectedPerformanceReportData.map((row) => {
+                                        return { name: row.source, value: Math.round(row.revenue) }
+                                    })
+                                })()
+                                }
+                            />
+                        </DashboardItem>
+
+                        <DashboardItem
+                            heading={`${t("conversions")}`}
+                            behavior={{
+                                lg: {row: selectedPerformanceReportData.length > 6 ? 4 : 2, col: 4},
+                                md: {row: selectedPerformanceReportData.length > 6 ? 4 : 2, col: 4},
+                                sm: {row: selectedPerformanceReportData.length > 6 ? 6 : 2, col: 4},
+                                default: {row: selectedPerformanceReportData.length > 6 ? 10 : 2, col: 4},
+                            }}
+                        >
+                            <ConversionsSankeyChart />
+                        </DashboardItem>
+                        
+
                     </DashboardContainer>
                 </Container>
             </BasicContentWrapper>
