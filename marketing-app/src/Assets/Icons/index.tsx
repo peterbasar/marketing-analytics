0eb0316f4625dc3b623ec84efd933e3fdae43459
PeterKillerio <peter.basar0@gmail.com>
2023-04-25 07:42:44 +0200

    <NEW> Globe icon

--- marketing-app/src/Assets/Icons/index.tsx
+++ marketing-app/src/Assets/Icons/index.tsx
@@ -3,4 +3,5 @@ export { ReactComponent as GridIcon } from './grid.svg';
 export { ReactComponent as AnalyticsIcon } from './analytics.svg';
 export { ReactComponent as CloseIcon } from './close.svg';
 export { ReactComponent as MenuIcon } from './menu.svg';
-export { ReactComponent as FileTray } from './file-tray-full.svg';
+export { ReactComponent as FileTrayIcon } from './file-tray-full.svg';
+export { ReactComponent as GlobeIcon } from './globe.svg';
