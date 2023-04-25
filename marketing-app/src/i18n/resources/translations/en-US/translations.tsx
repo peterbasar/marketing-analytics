b348f380661fc76dca8445790a12ccf20379fcd7
PeterKillerio <peter.basar0@gmail.com>
2023-04-25 06:08:42 +0200

    <EDIT> i18n

--- marketing-app/src/i18n/resources/translations/en-US/translations.tsx
+++ marketing-app/src/i18n/resources/translations/en-US/translations.tsx
@@ -3,8 +3,14 @@ import { translationInterface } from "i18n/resources/translations/interface"
 
 export const translations = {
     translation: {
+        /* Page link names */
+        select_api_link: "Select API",
+        select_partition_link: "Select partition",
+        dashboard_link: "Dashboard",
+
         dashboard_page_heading: "Dashboard",
-        partition_page_heading: "Available partitions",
+        partition_page_heading: "Available Partitions",
+        partition_data_page_heading: "Partition Data",
         
         enter_your_api_key: "Enter your API key",
 
@@ -19,6 +25,8 @@ export const translations = {
         roas: "ROAS",
         cpa: "CPA",
         conversions: "Conversions",
+        incrementality: "Paid channels",
+
 
         date_range: "Date range",
         date_range_start: "Start date",
@@ -28,7 +36,11 @@ export const translations = {
         brand: "Name",
         id: "ID",
         country: "Country",
+        column_date: "Date",
         column_source: "Source",
+        column_source_type: "Source type",
+        column_partition_id: "Parition ID",
+        column_optimisation_target: "Model type",
         column_revenue: "Revenue",
         column_conversions: "Conversions",
         column_spend: "Spent",
