2b793999abf890014b01ad804a2575c3f9d35007
PeterKillerio <peter.basar0@gmail.com>
2023-04-25 02:43:45 +0200

    <EDIT> RevenuSpendBarChart cleaning

--- marketing-app/src/Components/Charts/RevenueSpendBarChart/RevenueSpendBarChart.tsx
+++ marketing-app/src/Components/Charts/RevenueSpendBarChart/RevenueSpendBarChart.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from "react-i18next";
 /* Recharts */
 import {
     BarChart,
-    Scatter,
     XAxis,
     YAxis,
     CartesianGrid,
@@ -17,72 +16,42 @@ import {
 import { useDataManagerStore } from "Components/DataManager/DataManager.store";
 /* Components */
 import { ThemeContext } from "Theme/Theme";
+import addSpacesToNumber from "Utils/addSpacesToNumber";
 
-
-const data = [
-    {
-      "name": "Page A",
-      "source": 4000,
-      "revenue": 2400
-    },
-    {
-      "name": "Page B",
-      "source": 3000,
-      "revenue": 1398
-    },
-    {
-      "name": "Page C",
-      "source": 2000,
-      "revenue": 9800
-    },
-    {
-      "name": "Page D",
-      "source": 2780,
-      "revenue": 3908
-    },
-    {
-      "name": "Page E",
-      "source": 1890,
-      "revenue": 4800
-    },
-    {
-      "name": "Page F",
-      "source": 2390,
-      "revenue": 3800
-    },
-    {
-      "name": "Page G",
-      "source": 3490,
-      "revenue": 4300
-    }
-  ]
 
 const RevenueSpendBarChart = () => {
   /* Zustand */
-  const getRevenueSpendData = useDataManagerStore((state) => state.getRevenueSpendData)
-  const selectedSources = useDataManagerStore((state) => state.selectedSources)
+  const selectedPerformanceReportData = useDataManagerStore((state) => state.selectedPerformanceReportData)
   
   const theme = useContext(ThemeContext)
-
-  /* A. Get rows */
-  let revenueSpendData = getRevenueSpendData()
-
-  /* B. Filter out unselected sources */
-  revenueSpendData = revenueSpendData.filter((row) => selectedSources.includes(row.source))
-
-  /* C. Translate before passing */
   const { t } = useTranslation()
-  revenueSpendData.forEach((item) => {
-    item.source = t(item.source)
+
+  /* A. Get filtered data and format it */
+  let revenueSpendData: Array<{
+    source: string,
+    spend: number,
+    revenue: number,
+  }> = selectedPerformanceReportData.map((row) => {
+    return {
+      source: t(row.source),
+      spend: Math.round(row.spend),
+      revenue: Math.round(row.revenue),
+    }
   })
 
   return (
       <ResponsiveContainer width="100%" height="100%">
-          <BarChart width={730} height={250} data={revenueSpendData}>
+          <BarChart width={730} height={250} data={revenueSpendData}
+            margin={{ top: 20, right: 10, bottom: 10, left: 20 }}
+          >
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="source" />
               <YAxis />
-              <Tooltip />
+              <Tooltip
+                formatter={(value: any, name: any): [string, string] => {
+                  return [addSpacesToNumber(value), t(name)]
+                }}
+              />
               <Legend formatter={(value) => t(value)} />
               <Bar dataKey="spend" fill={theme.hex.secondary} />
               <Bar dataKey="revenue" fill={theme.hex.tertiary} />
